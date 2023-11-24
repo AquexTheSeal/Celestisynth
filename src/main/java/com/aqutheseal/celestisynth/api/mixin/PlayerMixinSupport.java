f1295d0a4c94582c806a088ca429ceeede500752
Meme Man <81907436+RaveTr@users.noreply.github.com>
2023-11-24 13:26:15 +0200

    more things

--- src/main/java/com/aqutheseal/celestisynth/api/mixin/PlayerMixinSupport.java
+++ src/main/java/com/aqutheseal/celestisynth/api/mixin/PlayerMixinSupport.java
@@ -5,22 +5,16 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 
 public interface PlayerMixinSupport {
 
-    @OnlyIn(Dist.CLIENT)
     int getScreenShakeDuration();
 
-    @OnlyIn(Dist.CLIENT)
     void setScreenShakeDuration(int duration);
 
-    @OnlyIn(Dist.CLIENT)
     int getScreenShakeFadeoutBegin();
 
-    @OnlyIn(Dist.CLIENT)
     void setScreenShakeFadeoutBegin(int duration);
 
-    @OnlyIn(Dist.CLIENT)
     float getScreenShakeIntensity();
 
-    @OnlyIn(Dist.CLIENT)
     void setScreenShakeIntensity(float intensity);
 
     int getCameraAngleOrdinal();
